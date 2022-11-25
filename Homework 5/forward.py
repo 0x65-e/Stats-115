41c9b6f7423957feed819282e699100e5bf13c17
0x65-e <16930669+0x65-e@users.noreply.github.com>
2022-11-25 14:35:48 -0800

    Homework 5 solution

--- Homework 5/forward.py
+++ Homework 5/forward.py
@@ -8,12 +8,14 @@ def expect(xDistribution, function):
 
 
 def forward(xT_1Distribution, eT, transitionTable, sensorTable):
-    
-##################################################
-#		Your code here
-################################################## 
-
-    return
+    # Calculate the prior distribution of xT only based on xT_1 and the transition function
+    prior = { next_state: expect(xT_1Distribution, lambda state: transitionTable[state][next_state]) for next_state in xT_1Distribution.keys() }
+    # Update the prior using Bayes' rule based on the sensor function and evidence
+    posterior = { state: sensorTable[state][eT] * prior[state] for state in prior.keys() }
+    # Renormalize the posterior to sum to 1
+    alpha = sum(posterior.values())
+    normalized_posterior = { state: prob / alpha for state, prob in posterior.items() }
+    return normalized_posterior
 
 def main():
     
--- Homework 5/testForward.py
+++ Homework 5/testForward.py
@@ -3,7 +3,7 @@ sys.path.append('../src/')
 
 import unittest
 from ddt import ddt, data, unpack
-import forward_YourLastName_YourFirstName as targetCode #change to file name
+import forward as targetCode
 
 
 @ddt
@@ -13,22 +13,12 @@ class TestForward(unittest.TestCase):
         self.assertEqual(calculatedDictionary.keys(), expectedDictionary.keys())
         for key in calculatedDictionary.keys():
             self.assertAlmostEqual(calculatedDictionary[key], expectedDictionary[key], places=places)
-
-##################################################
-#		Complete the code below
-##################################################  
                 
-    @data()#(xT_1Distribution, eT, transitionTable, sensorTable, expectedResult)
+    @data(({0: 0.435, 1: 0.278, 2: 0.287}, 0, {0: {0: 0.423, 1: 0.333, 2: 0.244}, 1: {0: 0.065, 1: 0.216, 2: 0.719}, 2: {0: 0.423, 1: 0.145, 2: 0.432}}, {0: {0: 0.05, 1: 0.95}, 1: {0: 0.4, 1: 0.6}, 2: {0: 0.85, 1: 0.15}}, {0: 0.0336753447580515, 1: 0.20530929377302407, 2: 0.7610153614689245}), ({0: 0.513, 1: 0.343, 2: 0.144}, 0, {0: {0: 0.262, 1: 0.488, 2: 0.250}, 1: {0: 0.659, 1: 0.178, 2: 0.163}, 2: {0: 0.376, 1: 0.352, 2: 0.272}}, {0: {0: 0.85, 1: 0.15}, 1: {0: 0.25, 1: 0.75}, 2: {0: 0.30, 1: 0.70}}, {0: 0.6910887042646321, 1: 0.1775214884808564, 2: 0.13138980725451152})) #(xT_1Distribution, eT, transitionTable, sensorTable, expectedResult)
     @unpack
     def test_forward(self, xT_1Distribution, eT, transitionTable, sensorTable, expectedResult):
-        
-        
-        
+        calculatedResult = targetCode.forward(xT_1Distribution, eT, transitionTable, sensorTable)
         self.assertNumericDictAlmostEqual(calculatedResult, expectedResult, places=4)
-
-##################################################
-#		Complete the code above
-##################################################  
 	
     def tearDown(self):
        pass
